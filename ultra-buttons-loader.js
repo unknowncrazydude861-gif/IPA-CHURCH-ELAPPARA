3f79963d6bae789f81cc793531ad97b43805fa5e
Unknown crazy dude 861 <unknowncrazydude861@gmail.com>
2026-09-14 22:54:57 +0530

    Load normal Visit button override

--- ultra-buttons-loader.js
+++ ultra-buttons-loader.js
@@ -6,3 +6,12 @@ if (!document.querySelector('link[data-ultra-buttons]')) {
   link.dataset.ultraButtons = 'true';
   document.head.appendChild(link);
 }
+
+// The navigation Visit CTA intentionally stays normal.
+if (!document.querySelector('link[data-normal-visit]')) {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = './visit-normal.css?v=20260914';
+  link.dataset.normalVisit = 'true';
+  document.head.appendChild(link);
+}
